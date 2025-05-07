ef90eee442440d3c76f4481598a14e50b1e68293
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-05-07 11:24:48 +0200

    Ajout de Scaffold pour le scelette de l'application

--- project/PassionLecture/flutter_application_1/lib/main.dart
+++ project/PassionLecture/flutter_application_1/lib/main.dart
@@ -4,16 +4,42 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar:
-            AppBar(backgroundColor: Colors.green, title: const Text('FLuuter')),
-      ),
-    );
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,
+        )),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text("ReadMe"),
+            ),
+          ),
+          bottomNavigationBar: NavigationBar(
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.book), label: 'Books'),
+              NavigationDestination(icon: Icon(Icons.home), label: 'home')
+            ],
+            onDestinationSelected: (int value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            selectedIndex: selectedIndex,
+          ),
+        ));
   }
 }
