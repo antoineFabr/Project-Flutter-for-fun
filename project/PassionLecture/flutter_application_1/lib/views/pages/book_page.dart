3c6eda3ab7f18818e8659c5d07160555622edb2d
antoineFabr <163823977+antoineFabr@users.noreply.github.com>
2025-05-08 11:22:01 +0200

    Ajout de check box

--- project/PassionLecture/flutter_application_1/lib/views/pages/book_page.dart
+++ project/PassionLecture/flutter_application_1/lib/views/pages/book_page.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
 
-class BookPage extends StatelessWidget {
+class BookPage extends StatefulWidget {
   const BookPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text("Book Page"),
+  State<BookPage> createState() => _BookPageState();
+}
 
+class _BookPageState extends State<BookPage> {
+  TextEditingController controller = TextEditingController();
+  bool? isCheck = false;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            onEditingComplete: () {
+              setState(() {
+                
+              });
+            },
+          ),
+          Text(controller.text),
+          Checkbox(
+            tristate: true,
+            value: isCheck,
+            onChanged: (bool? value) {
+            setState(() {
+              isCheck = value;
+            });
+          },),
+          CheckboxListTile(
+            tristate: true,
+            title: Text("click here"),
+            value: isCheck, 
+            onChanged: (bool? value) {
+            setState(() {
+              isCheck = value;
+            });},
+            )
+        ],
+        
+      ),
     );
   }
 }
